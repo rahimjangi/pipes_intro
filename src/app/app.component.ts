9e0b4dbaa63dd032ad4b486996d8467f714231be
rahim jangi <rahimjangi1982@yahoo.com>
2022-05-21 19:37:04 +0200

    final commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,35 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+  
 export class AppComponent {
-  title = 'pipes_intro';
+  datePipe = '';
+  upperCasePipe = '';
+  lowerCasePipe = '';
+  currencyPipe = '';
+  decimalPipe = '';
+  percentPipe = '';
+
+  onNameChange(e:any) {
+    this.datePipe = e.target.value;
+  }
+
+  getDatePipe(e:any) {
+    this.datePipe = e.target.value;
+  }
+  getUpperCasePipe(e: any) {
+    this.upperCasePipe = e.target.value;
+  }
+  getLowerCasePipe(e: any) {
+    this.lowerCasePipe = e.target.value;
+  }
+  getCurrencyPipe(e: any) {
+    this.currencyPipe = e.target.value;
+  }
+  getDecimalPipe(e: any) {
+    this.decimalPipe = e.target.value;
+  }
+  getPercentPipe(e: any) {
+    this.percentPipe = e.target.value;
+  }
 }
